fea5937834a9916041024963ca16ee55d802f816
Robert Yan <mstr.raphael@gmail.com>
2026-04-13 16:14:18 +0800

    fix: change reason

--- crates/api/src/bin/task_worker.rs
+++ crates/api/src/bin/task_worker.rs
@@ -130,7 +130,11 @@ impl TaskExecutor for DefaultTaskExecutor {
                 for instance in cleanup_targets.drain(..) {
                     if let Err(err) = self
                         .agent_service
-                        .delete_instance(instance.id, None, "cleanup_task_inactive_user")
+                        .delete_instance(
+                            instance.id,
+                            None,
+                            "cleanup_task_cancelled_user_after_grace_period",
+                        )
                         .await
                     {
                         failed_instances += 1;
